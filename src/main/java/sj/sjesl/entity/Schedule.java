1834aafe7d6158f0259a6ede52b3e5bfa39f7fe2
vnddn <vnddnjs00@naver.com>
2021-11-21 17:36:12 +0900

    회원가입 및 메시지인증

--- src/main/java/sj/sjesl/entity/Schedule.java
+++ src/main/java/sj/sjesl/entity/Schedule.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Schedule {
+public class Schedule  {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "schedule_id")
     private Long id;
